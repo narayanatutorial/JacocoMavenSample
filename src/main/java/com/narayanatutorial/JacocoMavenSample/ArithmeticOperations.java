c7699801560304c45aea97ea327d9d6f7dde9a27
narayanatutorial <narayanaswamy.ragi@gmail.com>
2019-11-07 00:11:40 +0530

    initial commit

---
+++ src/main/java/com/narayanatutorial/JacocoMavenSample/ArithmeticOperations.java
@@ -0,0 +1,14 @@
+package com.narayanatutorial.JacocoMavenSample;
+
+public class ArithmeticOperations {
+
+	public Integer add(Integer a, Integer b)
+	{
+		return a+b;
+	}
+
+	public Integer sub(Integer a, Integer b)
+	{
+		return a-b;
+	}
+}
